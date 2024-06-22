e96f2b221a9eca0c9ff9d10e2fdc78b93b1443e0
ngoctechno <luuxuanngoc2312@gmail.com>
2024-06-22 14:36:01 +0700

    update

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 require("dotenv").config();
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
+  next();
+});
 const app = express();
 app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 // Connect Mongoose
 mongoose
