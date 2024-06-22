2c105bd8c63baba0588231dddcd8a26514f7c3db
ngoctechno <luuxuanngoc2312@gmail.com>
2024-06-22 14:26:16 +0700

    update

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
+app.use(cors());
 
 // Connect Mongoose
 mongoose
