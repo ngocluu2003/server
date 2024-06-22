d1aac033fdf4815732ea6bab4e65a2915785379b
ngoctechno <luuxuanngoc2312@gmail.com>
2024-06-22 14:52:54 +0700

    update

--- index.js
+++ index.js
@@ -4,31 +4,13 @@ const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 require("dotenv").config();
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
-app.use(cors());
 
 // Connect Mongoose
 mongoose
@@ -47,9 +29,12 @@ mongoose
 app.get("/", (req, res) => {
   res.send(
     `<h1 style="padding: 32px 120px; text-align: center; margin-top: 32px">
-      Trang web cho phép người dùng đăng ký, đăng nhập, khám phá nhiều khóa học và mua sắm.
+      SkillLynx allows users to sign up, log in, explore a wide range of
+      courses, and make purchases.
       <br />
-      Ứng dụng cũng có một bảng điều khiển quản trị, nơi các quản trị viên có thể đăng nhập, đăng ký, tạo khóa học, và xuất bản hoặc giữ chúng ở trạng thái chưa xuất bản.
+      The app also features an admin panel where
+      administrators can log in, sign up, create courses, and publish or keep
+      them unpublished.
     </h1>`
   );
 });
